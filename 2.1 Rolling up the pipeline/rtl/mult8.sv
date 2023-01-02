1673f3014048602d27e38410bdf1beb9ce59b8d3
tesa <tej.sanghavi@nordicsemi.no>
2023-01-02 12:42:36 +0100

    Add comments to describe the module.

--- 2.1 Rolling up the pipeline/rtl/mult8.sv
+++ 2.1 Rolling up the pipeline/rtl/mult8.sv
@@ -3,15 +3,15 @@
 module mult8
   ( input  var logic i_clk
 
-  , input  var logic [7:0] i_a
-  , input  var logic [7:0] i_b
+  , input  var logic [7:0] i_a // Integer with a fixed-point to the right of the LSB.
+  , input  var logic [7:0] i_b // Fixed point to the left of the MSB.
 
   , output var logic [7:0] o_product
   );
 
   logic [15:0] prod16_q;
 
-  assign o_product = prod16_q[15:8];
+  assign o_product = prod16_q[15:8]; // B scales A from 0 to 1.
 
   always_ff @(posedge i_clk)
     prod16_q <= i_a * i_b;
