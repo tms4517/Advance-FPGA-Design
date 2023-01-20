721a9f47a26e35d810764032e4f614a87fed48c1
tesa <tej.sanghavi@nordicsemi.no>
2023-01-20 12:34:32 +0100

    FF with a set and synchronous reset.

---
+++ 2.4 Impact of reset on area/rtl/setReset_2.sv
@@ -0,0 +1,25 @@
+`default_nettype none
+
+module setReset
+  ( input  var logic i_clk
+
+  , input  var logic i_dat1
+  , input  var logic i_dat2
+  , input  var logic i_dat3
+
+  , output var logic o_dat
+  );
+
+logic dat_q;
+
+assign o_dat = dat_q;
+
+always_ff @(posedge i_clk)
+  if (i_dat3)
+    dat_q <= '0;
+  else if (i_dat1)
+    dat_q <=  1'b1;
+  else
+    dat_q <=  i_dat2;
+
+endmodule
